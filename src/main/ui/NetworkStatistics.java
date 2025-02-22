376e7990f46da6fee5e6a820743a12c3707ccf99
Phattarachittra67070263 <67070263@kmitl.ac.th>
2025-02-22 23:27:49 +0700

    Create NetworkStatistics.java

---
+++ src/main/ui/NetworkStatistics.java
@@ -0,0 +1,39 @@
+package main.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class NetworkStatistics extends JInternalFrame implements ActionListener {
+    private JMenuBar menuBar;
+    private JMenu menu1, menu2, item1;
+    private JMenuItem item2, subitem1, subitem2;
+    public NetworkStatistics(){
+        super("Network Statistics | Network Monitor",false,true,false,false);
+        menuBar = new JMenuBar();
+        menu1 = new JMenu("File");
+        menu2 = new JMenu("Options");
+        item1 = new JMenu("Export");
+        item2 = new JMenuItem("Print");
+        subitem1 = new JMenuItem("CSV");
+        subitem2 = new JMenuItem("JSON");
+
+        menuBar.add(menu1);
+        menuBar.add(menu2);
+        menu1.add(item1);
+        menu1.add(item2);
+        item1.add(subitem1);
+        item1.add(subitem2);
+
+        setJMenuBar(menuBar);
+
+        setSize(new Dimension(600,500));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
+
