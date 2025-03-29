9abbc6b0edcceadb92a9fc8c7d258957279536b8
bureerak <pongphon_brr@hotmail.com>
2025-03-29 09:51:07 +0700

    Graph(Usable)

--- src/main/database/graph/DeviceFetch.java
+++ src/main/database/graph/DeviceFetch.java
@@ -1,10 +1,8 @@
 package main.database.graph;
 
 import main.database.DBCP;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -34,7 +32,7 @@ public class DeviceFetch implements DataFetcher{
     public void fetch(int profile_id) {
         devices = new ArrayList<>();
         time = new ArrayList<>();
-        String sql = "SELECT COUNT(mac) AS Device , datetime FROM device_info WHERE profile_id = ? GROUP BY datetime";
+        String sql = "SELECT COUNT(mac) AS Device , datetime FROM device_info WHERE profile_id = ? GROUP BY datetime ORDER BY datetime ASC";
         try (Connection con = DBCP.getConnection(); PreparedStatement statement = con.prepareStatement(sql)) {
             statement.setInt(1, profile_id);
             ResultSet rs = statement.executeQuery();
@@ -52,6 +50,20 @@ public class DeviceFetch implements DataFetcher{
 
     @Override
     public void fetchRange(int profile_id ,LocalDateTime start, LocalDateTime stop) {
-
+        devices = new ArrayList<>();
+        time = new ArrayList<>();
+        String sql = "SELECT COUNT(mac) AS Device , datetime FROM device_info WHERE datetime BETWEEN ? AND ? AND profile_id = ? GROUP BY datetime ORDER BY datetime ASC";
+        try (Connection con = DBCP.getConnection(); PreparedStatement stm = con.prepareStatement(sql)) {
+            stm.setTimestamp(1, Timestamp.valueOf(start));
+            stm.setTimestamp(2,Timestamp.valueOf(stop));
+            stm.setInt(3,profile_id);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                devices.add(rs.getInt("Device"));
+                time.add(rs.getTimestamp("datetime").toLocalDateTime().toLocalTime());
+            }
+        } catch (SQLException s) {
+            System.out.println(s.getMessage());
+        }
     }
 }
